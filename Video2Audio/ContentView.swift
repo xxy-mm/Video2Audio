31d3d96225f18fbe3393940f82fecbc425ea25f0
xxy-mm <xxy.moming@gmail.com>
2024-10-04 16:51:23 +0800

    Initial Commit

---
+++ Video2Audio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Video2Audio
+//
+//  Created by xxy-mm on 2024/10/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Video2Audio/Video2AudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Video2AudioApp.swift
+//  Video2Audio
+//
+//  Created by xxy-mm on 2024/10/4.
+//
+
+import SwiftUI
+
+@main
+struct Video2AudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
